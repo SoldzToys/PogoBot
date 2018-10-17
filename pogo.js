49ac1cf312ca7f726eda13dfb73b6a82dff7cef0
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-17 12:41:36 -0400

    Update pogo.js

--- pogo.js
+++ pogo.js
@@ -28,7 +28,7 @@ if (message.content.startsWith(`${prefix}pogoinfo`)) {
     .addField(`POGO's Facebook`, `https://www.facebook.com/officialpogomusic/`, true)
     .addField(`POGO's Website`, `http://www.pogomix.net/`, true)
     .addField(`POGO's Other YT Channel`, `https://www.youtube.com/channel/UCG3aXJ4HoDugcg_E0E4y2VA`, true)
-    .setImage("POGO", "https://i.imgur.com/kpteGYu.png")
+    .setImage("POGO", client.user.displayAvatarURL)
     .setFooter(`${pogo}`)
     .setThumbnail(pogo) 
     .setTimestamp();
