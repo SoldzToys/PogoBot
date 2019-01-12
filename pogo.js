5eec4bbac87c6fab60e1eedff3d7f921e0f3f898
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2019-01-11 21:30:56 -0500

    Update pogo.js

--- pogo.js
+++ pogo.js
@@ -186,6 +186,7 @@ if (message.content.toLowerCase().startsWith(`${prefix}serverinfo`)) {
 			  .setColor(0xc470fa)
 			  .addField("We don't want spam!", "Sorry but you can only use my commands in <#531606891575377921>!")
 			  if (message.channel.name !== 'pogo-bot-channel') return message.channel.send(nonoembed).then(msg => msg.delete(10000))
+			  if(message.author.id === '363499842607120384', '272450070559850498') return;
 			                  const status = {
         false: "Human",
         true: "Bot"
