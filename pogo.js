794b92fbdcbff448c0afbed0b489b4c4dd5a9da8
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2019-01-11 21:37:08 -0500

    Update pogo.js

--- pogo.js
+++ pogo.js
@@ -186,7 +186,7 @@ if (message.content.toLowerCase().startsWith(`${prefix}serverinfo`)) {
 			  let nonoembed = new Discord.RichEmbed()
 			  .setColor(0xc470fa)
 			  .addField("We don't want spam!", "Sorry but you can only use my commands in <#531606891575377921>!")
-			  if ((message.channel.name !== 'pogo-bot-channel') && if (message.author.id !== '363499842607120384', '272450070559850498')) return message.channel.send(nonoembed).then(msg => msg.delete(10000))\
+			  if ((message.channel.name !== 'pogo-bot-channel') && (message.author.id !== '363499842607120384', '272450070559850498')) return message.channel.send(nonoembed).then(msg => msg.delete(10000))\
 			                  const status = {
         false: "Human",
         true: "Bot"
