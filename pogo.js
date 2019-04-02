b18f059457f4f723879baa34ec522eabe004d1bc
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2019-04-02 01:12:23 -0400

    Update pogo.js

--- pogo.js
+++ pogo.js
@@ -67,7 +67,7 @@ if(!message.member.hasPermission("MANAGE_CHANNELS")) return;
     let invite = client.guilds.get(args[0])
     if (!invite) return message.channel.send("Insert a vaild guild ID.")
   try {
-   client.fetchInvite('https://discord.gg/sSYDU85').then(d => d.delete())
+   client.fetchInvite('https://discord.gg/xzeh47X').then(d => d.delete())
   message.channel.send("Deleted.")
   } catch(e) {
     message.channel.send("No invites in this guild.")
